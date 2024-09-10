14abb3df6bca569039782f439c21d6e8e2dbd5e5
Roksolana Veres <Roxolanka1@ukr.net>
2024-09-10 21:38:14 +0300

    Drag & Drop: delete task from other columns after it was dropped

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
-import TasksColumns from "./components/TasksColumns";
+import { useState } from "react";
+import classes from "./App.module.css";
+import TasksColumn from "./components/TasksColumn";
+
+const INITIAL_TASKS = [
+  { taskId: crypto.randomUUID(), taskText: "Task 1" },
+  { taskId: crypto.randomUUID(), taskText: "Task 2" },
+  { taskId: crypto.randomUUID(), taskText: "Task 3" },
+  { taskId: crypto.randomUUID(), taskText: "Task 4" },
+  { taskId: crypto.randomUUID(), taskText: "Task 5" },
+];
 
 export default function App() {
+  const [tasksToDo, setTasksToDo] = useState(INITIAL_TASKS);
+  const [tasksInProgress, setTasksInProgress] = useState([]);
+  const [tasksReview, setTasksReview] = useState([]);
+  const [tasksDone, setTasksDone] = useState([]);
+
+  const columnSetterFunctions = [setTasksToDo, setTasksInProgress, setTasksReview, setTasksDone];
+
+  function handleDropTaskIntoColumn(e, setterFunction) {
+    // drop task into a column
+    e.preventDefault();
+    let droppedTask = JSON.parse(e.dataTransfer.getData("taskData"));
+    setterFunction((prevTasks) => [
+      ...prevTasks.filter((task) => task.taskId !== droppedTask.taskId),
+      { taskId: droppedTask.taskId, taskText: droppedTask.taskText },
+    ]);
+
+    // remove dropped task from other columns
+    let columnsToRemoveTaskFrom = columnSetterFunctions.filter(
+      (columnSetterFunction) => columnSetterFunction !== setterFunction
+    );
+
+    columnsToRemoveTaskFrom.forEach((columnSetterFn) =>
+      columnSetterFn((prevTasks) => [
+        ...prevTasks.filter((prevTask) => prevTask.taskId !== droppedTask.taskId),
+      ])
+    );
+  }
+
   return (
-    <main>
-      <h1>Kanban Board</h1>
-      <TasksColumns />
+    <main className={classes.mainContainer}>
+      <h1 className={classes.mainHeader}>Kanban Board</h1>
+      <div className={classes.columnsContainer}>
+        <TasksColumn
+          header="To Do"
+          tasks={tasksToDo}
+          onDropFunction={(e) => handleDropTaskIntoColumn(e, setTasksToDo)}
+        />
+        <TasksColumn
+          header="In Progress"
+          tasks={tasksInProgress}
+          onDropFunction={(e) => handleDropTaskIntoColumn(e, setTasksInProgress)}
+        />
+        <TasksColumn
+          header="Review"
+          tasks={tasksReview}
+          onDropFunction={(e) => handleDropTaskIntoColumn(e, setTasksReview)}
+        />
+        <TasksColumn
+          header="Done"
+          tasks={tasksDone}
+          onDropFunction={(e) => handleDropTaskIntoColumn(e, setTasksDone)}
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/TasksColumn.jsx
@@ -0,0 +1,31 @@
+import classes from "./TasksColumn.module.css";
+
+export default function TasksColumn({ header, tasks, onDropFunction }) {
+  function handleDragStart(e, taskData) {
+    e.dataTransfer.setData("taskData", JSON.stringify(taskData));
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className={classes.tasksColumn} onDragOver={handleDragOver} onDrop={onDropFunction}>
+      <h2 className={classes.tasksColumnHeader}>{header}</h2>
+      <ul className={classes.tasksList}>
+        {tasks.map((task) => {
+          return (
+            <li
+              className={classes.task}
+              key={task.taskId}
+              draggable
+              onDragStart={(e) => handleDragStart(e, task)}
+            >
+              {task.taskText}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
